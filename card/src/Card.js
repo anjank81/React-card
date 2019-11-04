a4ddc137d1dc060947cbbc058ccd44d99b582aff
anjank81 <anjank81@gmail.com>
2019-11-04 11:16:48 +0530

    Implemented state

--- card/src/Card.js
+++ card/src/Card.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
 import style from "./Card.module.css";
 import Image1 from "./nature.jpg";
+import Button from "./Button.js";
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "a.m" };
+  }
+  handle = () => {
+    this.setState({ name: "p.m" });
+  };
   render() {
     return (
-      <div className={style.card}>
+      <div className={style.base}>
         <img src={Image1} alt="Nature Image" height="550px;" width="317px" />
-        <div className="icons">
+        <div className="container">
           <div className={style.weather}>{"12°"}</div>
           <div className={style.time}>
             <div> 05:07</div>
-            <div className={style.timeap}>{"p.m"}</div>
+            <div className={style.convention}>{this.state.name}</div>
             <br />
             <div className={style.day}>{"Saturday"}</div>
           </div>
         </div>
+        <button onClick={this.handle}>Change am to pm</button>
       </div>
     );
   }
