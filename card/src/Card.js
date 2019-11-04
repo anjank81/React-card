a43ac36f9e23a9cf20508901749bf7da5c2322a1
anjank81 <anjank81@gmail.com>
2019-11-04 11:28:56 +0530

    Removed br tag and Renamed class names

--- card/src/Card.js
+++ card/src/Card.js
@@ -10,7 +10,7 @@ class Card extends Component {
           <div className={style.weather}>{"12°"}</div>
           <div className={style.time}>
             <div> 05:07</div>
-            <div className={style.convention}>p.m</div>
+            <div className={style.convention}>{this.state.name}</div>
             <br />
             <div className={style.day}>{"Saturday"}</div>
           </div>
