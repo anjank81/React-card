35be19bba87a96f308a3fef3f22b7dbf1b163652
anjank81 <anjank81@gmail.com>
2019-11-07 18:44:34 +0530

    Made card as component

--- card/src/Card.js
+++ card/src/Card.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import style from "./Card.module.css";
 import Image1 from "./nature.jpg";
 import Button from "./Button.js";
+import Cardcomp from "./Cardcomp";
 class Card extends Component {
   constructor(props) {
     super(props);
@@ -22,27 +23,7 @@ class Card extends Component {
     return (
       <div className={style.api}>
         {this.state.users.map(usr => {
-          return (
-            <div className={style.main}>
-              <div className={style.base}>
-                <img
-                  src={usr.avatar_url}
-                  alt="Nature Image"
-                  height="550px;"
-                  width="317px"
-                />
-                <div className="container">
-                  <div className={style.weather}>{"12°"}</div>
-                  <div className={style.time}>
-                    <div> 05:07</div>
-                    <div className={style.convention}>{this.state.name}</div>
-                    <br />
-                    <div className={style.day}>{usr.login}</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
+          return <Cardcomp src={usr.avatar_url} login={usr.login} />;
         })}
       </div>
     );
---
+++ card/src/Cardcomp.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+import style from "./Card.module.css";
+
+class Cardcomp extends Component {
+  render() {
+    return (
+      <div>
+        <div className={style.main}>
+          <div className={style.base}>
+            <img
+              src={this.props.src}
+              alt="Nature Image"
+              height="550px;"
+              width="317px"
+            />
+            <div className="container">
+              <div className={style.weather}>{"12°"}</div>
+              <div className={style.time}>
+                <div> 05:07</div>
+                <div className={style.convention}>am</div>
+                <br />
+                <div className={style.day}>{this.props.login}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Cardcomp;
