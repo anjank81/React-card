76ed2e1d6e6eca9b617ad2c8c8ef7c25df13c712
anjank81 <anjank81@gmail.com>
2019-11-08 14:52:49 +0530

    Used Axios

--- card/src/Card.js
+++ card/src/Card.js
@@ -3,27 +3,30 @@ import style from "./Card.module.css";
 import Image1 from "./nature.jpg";
 import Button from "./Button.js";
 import Cardcomp from "./Cardcomp";
+import axios from "axios";
 class Card extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: "a.m", users: [], isLoading: true };
+    this.state = { name: "p.m", users: [], isLoading: true };
   }
-  componentDidMount = async () => {
-    const result = await fetch("https://api.github.com/users");
-    const resultJson = await result.json();
-    this.setState({
-      users: resultJson,
-      isLoading: false
+  componentDidMount() {
+    axios.get("https://api.github.com/users").then(res => {
+      const users = res.data;
+      this.setState({ users });
     });
-  };
-  handle = () => {
-    this.setState({ name: "p.m" });
-  };
+  }
+
   render() {
     return (
       <div className={style.api}>
         {this.state.users.map(usr => {
-          return <Cardcomp src={usr.avatar_url} login={usr.login} />;
+          return (
+            <Cardcomp
+              src={usr.avatar_url}
+              login={usr.login}
+              name={this.state.name}
+            />
+          );
         })}
       </div>
     );
--- card/src/Cardcomp.js
+++ card/src/Cardcomp.js
@@ -8,17 +8,12 @@ class Cardcomp extends Component {
       <div>
         <div className={style.main}>
           <div className={style.base}>
-            <img
-              src={this.props.src}
-              alt="Nature Image"
-              height="550px;"
-              width="317px"
-            />
+            <img src={this.props.src} height="550px;" width="317px" />
             <div className="container">
               <div className={style.weather}>{"12°"}</div>
               <div className={style.time}>
                 <div> 05:07</div>
-                <div className={style.convention}>am</div>
+                <div className={style.convention}>{this.props.name}</div>
                 <br />
                 <div className={style.day}>{this.props.login}</div>
               </div>
