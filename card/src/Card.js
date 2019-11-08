0af5183cb1ea384fbdd1c0e298f42dbeccef8a65
anjank81 <anjank81@gmail.com>
2019-11-08 15:11:38 +0530

    Used Axios and added condition

--- card/src/Card.js
+++ card/src/Card.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import style from "./Card.module.css";
-import Image1 from "./nature.jpg";
-import Button from "./Button.js";
 import Cardcomp from "./Cardcomp";
 import axios from "axios";
 class Card extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: "p.m", users: [], isLoading: true };
+    this.state = { users: [], isLoading: true };
   }
   componentDidMount() {
     axios.get("https://api.github.com/users").then(res => {
@@ -19,15 +17,17 @@ class Card extends Component {
   render() {
     return (
       <div className={style.api}>
-        {this.state.users.map(usr => {
-          return (
-            <Cardcomp
-              src={usr.avatar_url}
-              login={usr.login}
-              name={this.state.name}
-            />
-          );
-        })}
+        {this.state.users &&
+          this.state.users.map(usr => {
+            return (
+              <Cardcomp
+                src={usr.avatar_url}
+                login={usr.login}
+                name={this.state.name}
+                key={usr.login}
+              />
+            );
+          })}
       </div>
     );
   }
